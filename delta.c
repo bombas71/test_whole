a917248a3862b7508c3885950e0fd24b75eda9b2
Giannopoulos Evangelos <e.giannopoulos@intralot.com>
2021-07-27 09:25:18 +0300

    first commit

---
+++ delta.c
@@ -0,0 +1,6 @@
+#include <stdio.h>  
+
+int main(){
+    printf("Hello World! \n");
+    printf("Hello World! \n");/// mekapoia comments kai to b rename me to remote
+}   
---
+++ gamma.c
@@ -0,0 +1,8 @@
+#include <stdio.h>  
+
+int main(){
+    printf("Hello World! \n");
+    printf("Hello World! \n");
+    printf("Hello World! \n");///comments
+    printf("Hello World! \n");// me kapoia comments
+}   
